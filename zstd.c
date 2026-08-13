6eb095e36222a148428b9fb13b926859e3b8972c
Boris Mironov <boris_mironov@outlook.com>
2026-08-13 21:19:13 +0700

    Zstd module refactoring into PG_TRY / PG_CATCH

--- zstd.c
+++ zstd.c
@@ -38,83 +38,89 @@ pg_zstd_free(void *opaque, void *address)
 Datum
 pg_zstd(PG_FUNCTION_ARGS)
 {
-	struct varlena *in_varlena = PG_GETARG_VARLENA_PP(0);
+	struct varlena *volatile in_varlena = PG_GETARG_VARLENA_PP(0);
 	int32 compression_level = PG_GETARG_INT32(1);
 	int16 threads = PG_GETARG_INT16(2);
 	const uint8 *in_data = (uint8 *)(VARDATA_ANY(in_varlena));
 	size_t in_size = VARSIZE_ANY_EXHDR(in_varlena);
 
-	uint8 *out_buf = NULL;
+	uint8 *volatile out_buf = NULL;
+	ZSTD_CCtx *volatile cctx = NULL;
+
 	struct varlena *out_varlena = NULL;
 	size_t comp_size = 0, max_dst_size = 0;
 	char *dst_buf = NULL;
 
-	ZSTD_CCtx *cctx;
 	ZSTD_customMem zstd_allocator = {
 			.customAlloc = pg_zstd_alloc,
 			.customFree = pg_zstd_free,
 			.opaque = (void *)CurrentMemoryContext};
 
-	// Determine safe upper bound for output buffer size
-	max_dst_size = ZSTD_compressBound(in_size);
-
-	if (in_size == 0) {
-		PG_FREE_IF_COPY(in_varlena, 0);
+	if (in_size == 0)
 		PG_RETURN_BYTEA_P(in_varlena);
+
+	PG_TRY();
+	{
+		if (in_size > max_uncompressed_size)
+			elog(ERROR,
+				 "input data is limited by pg_z.max_size (%zu bytes)",
+				 max_uncompressed_size);
+
+		if (!(compression_level >= 1 && compression_level <= 19))
+			elog(ERROR,
+				 "invalid compression level (outside of 1..19): %d",
+				 compression_level);
+
+		// Determine safe upper bound for output buffer size
+		max_dst_size = ZSTD_compressBound(in_size);
+
+		// Create a Zstd Context
+		cctx = ZSTD_createCCtx_advanced(zstd_allocator);
+		if (cctx == NULL)
+			elog(ERROR, "failed to create compression context");
+
+		// Allocate space for the PostgreSQL bytea output structure
+		max_dst_size += VARHDRSZ;
+		out_buf = (uint8 *)pg_hybrid_alloc(&max_dst_size);
+		if (out_buf == NULL)
+			elog(ERROR,
+				 "out of memory allocating %zu byte buffer",
+				 max_dst_size);
+
+		dst_buf = VARDATA(out_buf);
+
+		// Configure multi-threading parameters
+		ZSTD_CCtx_setParameter(
+				cctx, ZSTD_c_compressionLevel, compression_level);
+		ZSTD_CCtx_setParameter(cctx, ZSTD_c_nbWorkers, threads);
+
+		// Perform multi-threaded compression
+		comp_size = ZSTD_compressCCtx(
+				cctx,
+				dst_buf,
+				max_dst_size - VARHDRSZ,
+				in_data,
+				in_size,
+				compression_level);
+
+		// Check for runtime errors
+		if (ZSTD_isError(comp_size))
+			elog(ERROR, "compression error: %s", ZSTD_getErrorName(comp_size));
 	}
+	PG_CATCH();
+	{
+		PG_FREE_IF_COPY((struct varlena *)in_varlena, 0);
+		if (cctx != NULL)
+			ZSTD_freeCCtx(cctx);
+		if (out_buf != NULL)
+			pg_hybrid_free((void *)out_buf);
 
-	if (in_size > max_uncompressed_size) {
-		PG_FREE_IF_COPY(in_varlena, 0);
-		elog(ERROR,
-			 "input data is limited by pg_z.max_size (%zu bytes)",
-			 max_uncompressed_size);
+		PG_RE_THROW();
 	}
+	PG_END_TRY();
 
-	if (!(compression_level >= 1 && compression_level <= 19)) {
-		PG_FREE_IF_COPY(in_varlena, 0);
-		elog(ERROR,
-			 "invalid compression level (outside of 1..19): %d",
-			 compression_level);
-	}
-
-	// Create a Zstd Context
-	cctx = ZSTD_createCCtx_advanced(zstd_allocator);
-	if (cctx == NULL) {
-		PG_FREE_IF_COPY(in_varlena, 0);
-		elog(ERROR, "failed to create compression context");
-	}
-
-	// Allocate space for the PostgreSQL bytea output structure
-	max_dst_size += VARHDRSZ;
-	out_buf = (uint8 *)pg_hybrid_alloc(&max_dst_size);
-	if (out_buf == NULL) {
-		ZSTD_freeCCtx(cctx); // Free context to prevent memory leak
-		PG_FREE_IF_COPY(in_varlena, 0);
-		pg_hybrid_free(out_buf);
-		elog(ERROR, "out of memory allocating %zu byte buffer", max_dst_size);
-	}
-	dst_buf = VARDATA(out_buf);
-
-	// Configure multi-threading parameters
-	ZSTD_CCtx_setParameter(cctx, ZSTD_c_compressionLevel, compression_level);
-	ZSTD_CCtx_setParameter(cctx, ZSTD_c_nbWorkers, threads);
-
-	// Perform multi-threaded compression
-	comp_size = ZSTD_compressCCtx(
-			cctx,
-			dst_buf,
-			max_dst_size - VARHDRSZ,
-			in_data,
-			in_size,
-			compression_level);
+	PG_FREE_IF_COPY((struct varlena *)in_varlena, 0);
 	ZSTD_freeCCtx(cctx);
-	PG_FREE_IF_COPY(in_varlena, 0);
-
-	// Check for runtime errors
-	if (ZSTD_isError(comp_size)) {
-		pg_hybrid_free(out_buf);
-		elog(ERROR, "compression error: %s", ZSTD_getErrorName(comp_size));
-	}
 
 	out_varlena = (struct varlena *)out_buf;
 	SET_VARSIZE(out_varlena, comp_size + VARHDRSZ);
@@ -129,74 +135,86 @@ pg_zstd(PG_FUNCTION_ARGS)
 Datum
 pg_unzstd(PG_FUNCTION_ARGS)
 {
-	struct varlena *in_varlena = PG_GETARG_VARLENA_PP(0);
+	struct varlena *volatile in_varlena = PG_GETARG_VARLENA_PP(0);
 	const uint8 *in_data = (uint8 *)(VARDATA_ANY(in_varlena));
 	size_t in_size = VARSIZE_ANY_EXHDR(in_varlena);
 
-	uint8 *out_buf = NULL;
+	uint8 *volatile out_buf = NULL;
+	ZSTD_DCtx *volatile dctx = NULL;
+
 	struct varlena *out_varlena = NULL;
 	char *dst_buf = NULL;
-	size_t uncomp_size = 0;
+	size_t uncomp_size = 0, uncompressed_size = 0;
 
-	ZSTD_DCtx *dctx;
 	ZSTD_customMem zstd_allocator = {
 			.customAlloc = pg_zstd_alloc,
 			.customFree = pg_zstd_free,
 			.opaque = (void *)CurrentMemoryContext};
 
-	// Find out the original uncompressed frame size
-	size_t uncompressed_size = ZSTD_getFrameContentSize(in_data, in_size);
-
-	if (in_size == 0) {
+	if (in_size == 0)
 		PG_RETURN_BYTEA_P(in_varlena);
-	}
 
-	if (uncompressed_size == ZSTD_CONTENTSIZE_ERROR) {
-		PG_FREE_IF_COPY(in_varlena, 0);
-		elog(ERROR, "decompression error: Not a valid compressed Zstd frame");
-	}
-	if (uncompressed_size == ZSTD_CONTENTSIZE_UNKNOWN) {
-		PG_FREE_IF_COPY(in_varlena, 0);
-		elog(ERROR, "decompression error: Uncompressed size unknown");
-	}
+	PG_TRY();
+	{
+		// Find out the original uncompressed frame size
+		uncompressed_size = ZSTD_getFrameContentSize(in_data, in_size);
 
-	// Instantiate Decompression Context using our Postgres palloc wrapper
-	dctx = ZSTD_createDCtx_advanced(zstd_allocator);
-	if (dctx == NULL) {
-		PG_FREE_IF_COPY(in_varlena, 0);
-		elog(ERROR, "failed to allocate decompression context using palloc");
+		if (uncompressed_size == ZSTD_CONTENTSIZE_ERROR)
+			elog(ERROR,
+				 "decompression error: Not a valid compressed Zstd frame");
+
+		if (uncompressed_size == ZSTD_CONTENTSIZE_UNKNOWN)
+			elog(ERROR, "decompression error: Uncompressed size unknown");
+
+		if (uncompressed_size > max_uncompressed_size)
+			elog(ERROR,
+				 "decompressed output will exceed pg_z.max_size (%zu bytes)",
+				 max_uncompressed_size);
+
+		// Instantiate Decompression Context using our Postgres palloc wrapper
+		dctx = ZSTD_createDCtx_advanced(zstd_allocator);
+		if (dctx == NULL)
+			elog(ERROR,
+				 "failed to allocate decompression context using palloc");
+
+		// Allocate memory for uncompressed result
+		uncompressed_size += VARHDRSZ;
+		out_buf = (uint8 *)pg_hybrid_alloc(&uncompressed_size);
+		if (out_buf == NULL)
+			elog(ERROR,
+				 "out of memory allocating %zu byte buffer",
+				 (size_t)uncompressed_size);
+
+		dst_buf = VARDATA(out_buf);
+
+		// Decompress frame
+		uncomp_size = ZSTD_decompress(
+				dst_buf, uncompressed_size - VARHDRSZ, in_data, in_size);
+
+		if (uncomp_size > max_uncompressed_size)
+			elog(ERROR,
+				 "decompressed output exceeds pg_z.max_size (%zu bytes)",
+				 max_uncompressed_size);
+
+		if (ZSTD_isError(uncomp_size))
+			elog(ERROR,
+				 "decompression error: %s",
+				 ZSTD_getErrorName(uncomp_size));
 	}
+	PG_CATCH();
+	{
+		PG_FREE_IF_COPY((struct varlena *)in_varlena, 0);
+		if (dctx != NULL)
+			ZSTD_freeDCtx(dctx);
+		if (out_buf != NULL)
+			pg_hybrid_free((void *)out_buf);
 
-	// Allocate memory for uncompressed result
-	uncompressed_size += VARHDRSZ;
-	out_buf = (uint8 *)pg_hybrid_alloc(&uncompressed_size);
-	if (out_buf == NULL) {
-		ZSTD_freeDCtx(dctx); // Safely frees through pg_zstd_free wrapper
-		PG_FREE_IF_COPY(in_varlena, 0);
-		elog(ERROR,
-			 "out of memory allocating %zu byte buffer",
-			 (size_t)uncompressed_size);
+		PG_RE_THROW();
 	}
-	dst_buf = VARDATA(out_buf);
+	PG_END_TRY();
 
-	// Decompress frame
-	uncomp_size = ZSTD_decompress(
-			dst_buf, uncompressed_size - VARHDRSZ, in_data, in_size);
-
+	PG_FREE_IF_COPY((struct varlena *)in_varlena, 0);
 	ZSTD_freeDCtx(dctx);
-	PG_FREE_IF_COPY(in_varlena, 0);
-
-	if (uncomp_size > max_uncompressed_size) {
-		pg_hybrid_free(out_buf);
-		elog(ERROR,
-			 "decompressed output exceeds pg_z.max_size (%zu bytes)",
-			 max_uncompressed_size);
-	}
-
-	if (ZSTD_isError(uncomp_size)) {
-		pg_hybrid_free(out_buf);
-		elog(ERROR, "decompression error: %s", ZSTD_getErrorName(uncomp_size));
-	}
 
 	out_varlena = (struct varlena *)out_buf;
 	SET_VARSIZE(out_varlena, uncomp_size + VARHDRSZ);
