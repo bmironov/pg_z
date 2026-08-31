61390e107e0cdea2bb779af974c147531fb4947e
bmironov <boris.mironov@gmail.com>
2026-08-31 17:06:29 +0700

    Update build-defaults.sh

--- build/build-defaults.sh
+++ build/build-defaults.sh
@@ -64,7 +64,7 @@ get_os_mirror() {
         # echo "http://cdn-aws.deb.debian.org"
         ;;
     $OS_FAMILY_UBUNTU)
-        echo "http://archive.ubuntu.org"
+        echo "http://archive.ubuntu.com"
         # echo "http://cdn-aws.archive.ubuntu.com"
         ;;
     *)
