5a1a9508ba4d40078a75b4638afe99bb7397d054
Boris Mironov <boris_mironov@outlook.com>
2026-09-08 22:45:30 +0700

    Fix: make installcheck

--- sql/core.sql
+++ sql/core.sql
@@ -14,34 +14,26 @@ SELECT to_hex(pg_z_version_num());
 --
 -- pg_z_details
 --
+\set algos $${"Brotli", "Gzip/Deflate", "Gzip-NG/Deflate-NG", "LZ4", "Snappy", "Zstd"}$$
+\set links $${"static", "dynamic"}$$
+
 SELECT COUNT(*) FROM pg_z_details();
-
-SELECT COUNT(*) FROM pg_z_details() WHERE algorithm='Unknown';
-
-SELECT COUNT(*)
-FROM pg_z_details()
-WHERE algorithm IN ('Brotli', 'GZip', 'Gzip-NG', 'LZ4', 'Snappy', 'Zstd')
-GROUP BY algorithm
-ORDER BY algorithm;
-
-SELECT COUNT(*)
-FROM pg_z_details()
-WHERE algorithm IN ('Brotli', 'GZip', 'Gzip-NG', 'LZ4', 'Snappy', 'Zstd')
-    AND linking IN ('static', 'dynamic')
-GROUP BY algorithm
-ORDER BY algorithm;
-
-SELECT COUNT(*)
-FROM pg_z_details()
-WHERE algorithm IN ('Brotli', 'GZip', 'Gzip-NG', 'LZ4', 'Snappy', 'Zstd')
-    AND linking NOT IN ('static', 'dynamic')
-GROUP BY algorithm
-ORDER BY algorithm;
 
 SELECT COUNT(*) FROM (
     SELECT unnest(array[algorithm, version, linking])
     FROM pg_z_details()
     WHERE algorithm='LZ4'
 );
+
+SELECT COUNT(*) FROM pg_z_details() WHERE algorithm='Unknown';
+
+SELECT COUNT(*) FROM pg_z_details() WHERE algorithm = ANY(:algos::TEXT[]);
+
+SELECT COUNT(*) FROM pg_z_details() WHERE algorithm = ANY(:algos::TEXT[]) AND linking = ANY(:links::TEXT[]);
+
+SELECT COUNT(*) FROM pg_z_details() WHERE algorithm != ALL(:algos::TEXT[]);
+
+SELECT COUNT(*) FROM pg_z_details() WHERE linking != ALL(:links::TEXT[]);
+
 
 DROP EXTENSION pg_z;
